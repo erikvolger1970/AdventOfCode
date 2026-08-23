36872c9fa3720be32ddb0cd516bb1ccbd811c720
Erik Volger <erikvolger1970@gmail.com>
2026-08-23 21:05:05 +0200

    added day1 of AdventOfCode 2021

--- AdventOfCode2025/Year2021/Day1.cs
+++ AdventOfCode2025/Year2021/Day1.cs
@@ -4,9 +4,25 @@ internal class Day1
 {
     public static void Run()
     {
-        
+        // read file and convertt into list of integers
+        IEnumerable<int> depths = File
+            .ReadAllText("Year2021/day1input.txt")
+            .Split()
+            .Select(ParseInt);
 
-        Console.WriteLine($"Solution = ?");
+        // count all the depth increases of a moveing window
+        int previous = depths.First();
+        var totalDepthIncreases = depths.Aggregate(0, (total, next) =>
+        {
+            int increase = next > previous ? 1 : 0;
+            previous = next;
+            return total + increase;
+        });
+
+        Console.WriteLine($"Solution = {totalDepthIncreases}"); // 1390
     }
 
+    // convert a string to an int, use 0 for empty strings
+    private static int ParseInt(string s) =>
+        int.TryParse(s, out int result) ? result : 0;
 }
