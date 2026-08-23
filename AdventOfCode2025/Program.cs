413f1bd3dd178e3e0e28dfe7e41feb39f67e673f
Erik Volger <erikvolger1970@gmail.com>
2026-08-23 19:28:04 +0200

    added comments

--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,12 +1,19 @@
 ﻿using AdventOfCode2025;
 
-var clicks = File
+// lees aangeleverde file in en converteer naar reeks rotaties.
+IEnumerable<int> clicks = File
     .ReadAllText("safecombinations.txt")
     .Split()
     .Select(line => ParseClicks(line));
 
 int solution = 0;
+
+// De Dial class is verantwoordelijk voor bijhouden van de positie tijdens de rotaties.
+// De rotaties kunnen meer dan één omwenteling naar links of rechts inhouden.
+// Met TDD ontwikkeld. De tests staan in AdventOfCode2025.Tests.csproj
 Dial dial = new(50);
+
+// Voer alle ingelezen rotaties door in de juiste volgorde en houdt bij hoe vaak de Dial op positie op 0 land.
 foreach (var click in clicks)
 {
     dial.Rotate(click);
