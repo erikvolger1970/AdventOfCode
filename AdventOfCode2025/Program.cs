aa996f116d47c4046bf152e9d7177c0d29e310fa
Erik Volger <erikvolger1970@gmail.com>
2026-08-23 19:13:53 +0200

    solution to day 1

--- AdventOfCode2025/Program.cs
+++ AdventOfCode2025/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Goodbye");
+﻿using AdventOfCode2025;
+
+var clicks = File
+    .ReadAllText("safecombinations.txt")
+    .Split()
+    .Select(line => ParseClicks(line));
+
+int solution = 0;
+Dial dial = new(50);
+foreach (var click in clicks)
+{
+    dial.Rotate(click);
+    if (dial.Position == 0)
+        solution++;
+}
+
+Console.WriteLine($"Solution = { solution}");
+
+static int ParseClicks(string rotation)
+{
+    if (string.IsNullOrEmpty(rotation))
+        return 0;
+
+    string number = rotation[0] switch
+    {
+        'R' => rotation[1..],
+        'L' => '-'+rotation[1..],
+        _ => "0"
+    };
+
+    return int.TryParse(number, out int result) ? result : 0;
+}
