2438793af884f3e23980d1e002e4cd358ab69dd5
Erik Volger <erikvolger1970@gmail.com>
2026-08-25 08:02:01 +0200

    calculate moves before executing them per herd

--- AdventOfCode2025/Year2021/Day25.cs
+++ AdventOfCode2025/Year2021/Day25.cs
@@ -40,7 +40,6 @@ public class Board
 
     private readonly ICellChecker _horizontalCellChecker;
     private readonly ICellChecker _verticalCellChecker;
-    private bool _somethingMoved = false;
 
     public Board(char[][] cells)
     {
@@ -54,43 +53,50 @@ public class Board
     
     public bool Step()
     {
-        _somethingMoved = false;
-        CheckCells(_horizontalCellChecker);
-        CheckCells(_verticalCellChecker);
-        return _somethingMoved;
+        IEnumerable<Move> horizontalMoves = CalculateMoves(_horizontalCellChecker);
+        ExecuteMoves(horizontalMoves);
+
+        IEnumerable<Move> verticalMoves = CalculateMoves(_verticalCellChecker);
+        ExecuteMoves(verticalMoves);
+
+        return horizontalMoves.Any() || verticalMoves.Any();
     }
 
-    private void CheckCells(ICellChecker cellChecker)    
+    private List<Move> CalculateMoves(ICellChecker cellChecker)    
     {
+        List<Move> moves = [];
+
         // use the same order for both horizontal and vertical creatures
         // it makes no difference that vertical creatures are processed by row first, while they move by col...
         for (int row = 0; row < _rows; row++)
             for (int col = 0; col < _columns; col++)
-                CheckCell(cellChecker, new Cell(row, col));
+            {
+                Cell currentCell = new(row, col);
+                if (HasCreature(currentCell, cellChecker.CreatureType))
+                {
+                    Cell adjacentCell = cellChecker.AdjacentCell(currentCell);
+                    if (IsEmpty(adjacentCell))
+                    {
+                        moves.Add(new Move(currentCell, adjacentCell));
+                    }
+                }
+            }
+
+        return moves;
     }
 
-    private void CheckCell(ICellChecker cellChecker, Cell current)
+    private void ExecuteMoves(IEnumerable<Move> moves)
     {
-        if (HasCreature(current, cellChecker.CreatureType))
+        foreach (Move move in moves)
         {
-            Cell cell = cellChecker.AdjacentCell(current);
-            if (IsEmpty(cell))
-            {
-                DoMove(new Move(current, cell));
-                _somethingMoved = true;
-            }
+            _cells[move.To.Row][move.To.Col] = _cells[move.From.Row][move.From.Col];
+            _cells[move.From.Row][move.From.Col] = '.';
         }
     }
 
     private bool HasCreature(Cell cell, char creatureType) => _cells[cell.Row][cell.Col] == creatureType;
 
     private bool IsEmpty(Cell cell) => _cells[cell.Row][cell.Col] == '.';
-
-    private void DoMove(Move move)
-    {        
-        _cells[move.To.Row][move.To.Col] = _cells[move.From.Row][move.From.Col];
-        _cells[move.From.Row][move.From.Col] = '.';
-    }
 
     private record Cell(int Row, int Col);
     private record Move(Cell From, Cell To);
